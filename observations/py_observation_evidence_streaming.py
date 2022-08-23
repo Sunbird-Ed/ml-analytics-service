e998e6925349b1314f6ea1893533d150c171b213
Snehangsu De <desnehangsu@gmail.com>
2022-08-23 23:54:50 +0530

    Observation Evidence removal MongoDB

--- observations/py_observation_evidence_streaming.py
+++ observations/py_observation_evidence_streaming.py
@@ -1,20 +1,17 @@
 # -----------------------------------------------------------------
 # Name : py_observation_evidence_streaming.py
-# Author : Shakthieshwari.A
+# Author : Shakthieshwari.A, Snehangsu, Ajay
 # Description : Extracts the Evidence or Files Attached at each question level 
-#               during the observation submission
+# during the observation submission
 # -----------------------------------------------------------------
-from pymongo import MongoClient
-from bson.objectid import ObjectId
-import os, json
-import datetime
-# from datetime import date, time
-from kafka import KafkaConsumer, KafkaProducer
-from configparser import ConfigParser, ExtendedInterpolation
+
 import faust
 import logging
-import logging.handlers
-from logging.handlers import TimedRotatingFileHandler
+import os, json
+import datetime
+from kafka import KafkaConsumer, KafkaProducer
+from configparser import ConfigParser, ExtendedInterpolation
+from logging.handlers import TimedRotatingFileHandler, RotatingFileHandler
 
 config_path = os.path.split(os.path.dirname(os.path.abspath(__file__)))
 config = ConfigParser(interpolation=ExtendedInterpolation())
@@ -25,29 +22,18 @@ formatter = logging.Formatter('%(asctime)s - %(levelname)s')
 successLogger = logging.getLogger('success log')
 successLogger.setLevel(logging.DEBUG)
 
-# Add the log message handler to the logger
-successHandler = logging.handlers.RotatingFileHandler(
-  config.get('LOGS','observation_streaming_evidence_success')
-)
-successBackuphandler = TimedRotatingFileHandler(
-  config.get('LOGS','observation_streaming_evidence_success'),
-  when="w0",
-  backupCount=1
-)
+# Handles success logs
+successHandler = RotatingFileHandler(config.get('LOGS','observation_streaming_evidence_success'))
+successBackuphandler = TimedRotatingFileHandler(config.get('LOGS','observation_streaming_evidence_success'),when="w0",backupCount=1)
 successHandler.setFormatter(formatter)
 successLogger.addHandler(successHandler)
 successLogger.addHandler(successBackuphandler)
 
+# Handles Error logs
 errorLogger = logging.getLogger('error log')
 errorLogger.setLevel(logging.ERROR)
-errorHandler = logging.handlers.RotatingFileHandler(
-  config.get('LOGS','observation_streaming_evidence_error')
-)
-errorBackuphandler = TimedRotatingFileHandler(
-  config.get('LOGS','observation_streaming_evidence_error'),
-  when="w0",
-  backupCount=1
-)
+errorHandler = logging.handlers.RotatingFileHandler(config.get('LOGS','observation_streaming_evidence_error'))
+errorBackuphandler = TimedRotatingFileHandler(config.get('LOGS','observation_streaming_evidence_error'),when="w0",backupCount=1)
 errorHandler.setFormatter(formatter)
 errorLogger.addHandler(errorHandler)
 errorLogger.addHandler(errorBackuphandler)
@@ -63,10 +49,7 @@ try:
   )
   rawTopicName = app.topic(config.get("KAFKA", "observation_raw_topic"))
   producer = KafkaProducer(bootstrap_servers=[kafka_url])
-  #db production
-  clientdev = MongoClient(config.get('MONGO','mongo_url'))
-  db = clientdev[config.get('MONGO','database_name')]
-  quesCollec = db[config.get('MONGO','questions_collection')]
+
 except Exception as e:
   errorLogger.error(e, exc_info=True)
 
@@ -78,18 +61,19 @@ except Exception as e:
 
 try:
   def evidence_extraction(obSub):
-    if 'isAPrivateProgram' in obSub :
-      successLogger.debug("Observation Evidence Submission Id : " + obSub['_id'])
-      try:
-        completedDate = obSub['completedDate']
-      except KeyError:
-        completedDate = '' 
-      evidence_sub_count = 0
+    if obSub['status']=='completed':
+      if 'isAPrivateProgram' in obSub :
+        successLogger.debug("Observation Evidence Submission Id : " + obSub['_id'])
+        try:
+          completedDate = obSub['completedDate']
+        except KeyError:
+          completedDate = '' 
+        evidence_sub_count = 0
 
       try:
         answersArr = [ v for v in obSub['answers'].values()]
       except KeyError:
-        pass
+        answersArr = []
 
       for ans in answersArr:
         try:
@@ -102,84 +86,85 @@ try:
 
     if completedDate :
       for answer in answersArr:
-       try:
-        if answer['qid']:
-          observationSubQuestionsObj = {}
-          observationSubQuestionsObj['completedDate'] = completedDate
-          observationSubQuestionsObj['total_evidences'] = evidence_sub_count
-          observationSubQuestionsObj['observationSubmissionId'] = str(obSub['_id'])
-          observationSubQuestionsObj['entity'] = str(obSub['entityId'])
-          observationSubQuestionsObj['entityExternalId'] = obSub['entityExternalId']
-          observationSubQuestionsObj['entityName'] = obSub['entityInformation']['name']
-          observationSubQuestionsObj['entityType'] = str(obSub['entityType'])
-          observationSubQuestionsObj['createdBy'] = obSub['createdBy']
-          observationSubQuestionsObj['solutionExternalId'] = obSub['solutionExternalId']
-          observationSubQuestionsObj['solutionId'] = str(obSub['solutionId'])
-          observationSubQuestionsObj['observationId'] = str(obSub['observationId'])
+        try:
+          if answer['qid']:
+            observationSubQuestionsObj = {}
+            observationSubQuestionsObj['completedDate'] = completedDate
+            observationSubQuestionsObj['total_evidences'] = evidence_sub_count
+            observationSubQuestionsObj['observationSubmissionId'] = str(obSub['_id'])
+            observationSubQuestionsObj['entity'] = str(obSub['entityId'])
+            observationSubQuestionsObj['entityExternalId'] = obSub['entityExternalId']
+            observationSubQuestionsObj['entityName'] = obSub['entityInformation']['name']
+            observationSubQuestionsObj['entityType'] = str(obSub['entityType'])
+            observationSubQuestionsObj['createdBy'] = obSub['createdBy']
+            observationSubQuestionsObj['solutionExternalId'] = obSub['solutionExternalId']
+            observationSubQuestionsObj['solutionId'] = str(obSub['solutionId'])
+            observationSubQuestionsObj['observationId'] = str(obSub['observationId'])
 
-          try :
-            observationSubQuestionsObj['appName'] = obSub["appInformation"]["appName"].lower()
-          except KeyError :
-            observationSubQuestionsObj['appName'] = config.get("ML_APP_NAME", "survey_app")
+            try :
+              observationSubQuestionsObj['appName'] = obSub["appInformation"]["appName"].lower()
+            except KeyError :
+              observationSubQuestionsObj['appName'] = config.get("ML_APP_NAME", "survey_app")
 
-          fileName = []
-          fileSourcePath = []
+            fileName = []
+            fileSourcePath = []
 
-          try:
-            observationSubQuestionsObj['remarks'] = answer['remarks']
-          except KeyError:
-            observationSubQuestionsObj['remarks'] = ''
-          observationSubQuestionsObj['questionId'] = str(answer['qid'])
+            try:
+              observationSubQuestionsObj['remarks'] = answer['remarks']
+            except KeyError:
+              observationSubQuestionsObj['remarks'] = ''
+            observationSubQuestionsObj['questionId'] = str(answer['qid'])
+            try:
+              observationSubQuestionsObj['questionExternalId'] = str(answer['externalId'])
+              observationSubQuestionsObj['questionName'] = answer['payload']['question'][0]
+            except KeyError:
+              observationSubQuestionsObj['questionExternalId'] = ''
+              observationSubQuestionsObj['questionName'] = ''
 
-          questionsCollec = quesCollec.find({'_id':ObjectId(observationSubQuestionsObj['questionId'])})
-          for ques in questionsCollec:
-            observationSubQuestionsObj['questionExternalId'] = ques['externalId']
-            observationSubQuestionsObj['questionName'] = ques["question"][0] 
-          observationSubQuestionsObj['questionResponseType'] = answer['responseType']
-          evidence = []
-          evidenceCount = 0
+            observationSubQuestionsObj['questionResponseType'] = answer['responseType']
+            evidence = []
+            evidenceCount = 0
 
-          try:
-            if answer['fileName']:
-              evidence = answer['fileName']
-              observationSubQuestionsObj['evidence_count'] = len(evidence)
-              evidenceCount = len(evidence)
-          except KeyError:
-            if answer['instanceFileName']:
-              for inst in answer['instanceFileName']:
-                evidence.extend(inst)
-              observationSubQuestionsObj['evidence_count'] = len(evidence)
-              evidenceCount = len(evidence)
+            try:
+              if answer['fileName']:
+                evidence = answer['fileName']
+                observationSubQuestionsObj['evidence_count'] = len(evidence)
+                evidenceCount = len(evidence)
+            except KeyError:
+              if answer['instanceFileName']:
+                for inst in answer['instanceFileName']:
+                  evidence.extend(inst)
+                observationSubQuestionsObj['evidence_count'] = len(evidence)
+                evidenceCount = len(evidence)
 
-          for evi in evidence:
-            fileName.append(evi['name'])
-            fileSourcePath.append(evi['sourcePath'])
-          observationSubQuestionsObj['fileName'] = convert(fileName)
-          observationSubQuestionsObj['fileSourcePath'] = convert(fileSourcePath)
-          if evidenceCount > 0:
-            producer.send(
-              (config.get("KAFKA", "observation_evidence_druid_topic")), 
-              json.dumps(observationSubQuestionsObj).encode('utf-8')
-            )     
-            producer.flush()
-            successLogger.debug("Send Obj to Kafka")
-       except KeyError:
-        pass
+            for evi in evidence:
+              fileName.append(evi['name'])
+              fileSourcePath.append(evi['sourcePath'])
+            observationSubQuestionsObj['fileName'] = convert(fileName)
+            observationSubQuestionsObj['fileSourcePath'] = convert(fileSourcePath)
+            if evidenceCount > 0:
+              producer.send(
+                (config.get("KAFKA", "observation_evidence_druid_topic")), 
+                json.dumps(observationSubQuestionsObj).encode('utf-8')
+              )     
+              producer.flush()
+              successLogger.debug("Send Obj to Kafka")
+        except KeyError:
+          pass
 except Exception as e:
   errorLogger.error(e, exc_info=True)
 
 try:
  @app.agent(rawTopicName)
  async def observationEvidenceFaust(consumer):
-   async for msg in consumer :
-     msg_val = msg.decode('utf-8')
-     msg_data = json.loads(msg_val)
-     if msg_data["status"] == "completed":
+    async for msg in consumer :
+      msg_val = msg.decode('utf-8')
+      msg_data = json.loads(msg_val)
       successLogger.debug("========== START OF OBSERVATION EVIDENCE SUBMISSION ========")
       evidence_extraction(msg_data)
       successLogger.debug("********* END OF OBSERVATION EVIDENCE SUBMISSION ***********")
 except Exception as e:
- errorLogger.error(e, exc_info=True)
+  errorLogger.error(e, exc_info=True)
 
 if __name__ == '__main__':
  app.main()
