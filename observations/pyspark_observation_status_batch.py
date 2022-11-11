b100474046e459c7b338c3da18f54f217c8dfb99
Shakthieshwari <shakthieshwari@shikshalokam.org>
2022-11-11 18:38:35 +0530

    exception handling for enabling the datasource

--- observations/pyspark_observation_status_batch.py
+++ observations/pyspark_observation_status_batch.py
@@ -681,7 +681,7 @@ for i,j in zip(datasources,ingestion_specs):
          )
          if delete_segments.status_code == 200:
             successLogger.debug("successfully deleted the segments " + i)
-            time.sleep(300)
+            time.sleep(600)
 
             enable_datasource = requests.get(druid_end_point, headers=headers)
             if enable_datasource.status_code == 200:
