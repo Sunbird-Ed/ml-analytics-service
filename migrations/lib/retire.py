64e43e7b3522883de9a1ecb173e41ddaaafdd63a
adithya_dinesh <adithya@shikshalokam.org>
2023-06-19 20:50:23 +0530

    Update reports and retire reports scripts

---
+++ migrations/lib/retire.py
@@ -0,0 +1,106 @@
+import requests
+import os, json,sys,csv,json
+from configparser import ConfigParser,ExtendedInterpolation
+from update import fetchAllReports
+
+# Read the Config
+config_path = os.path.split(os.path.dirname(os.path.abspath(__file__)))
+config = ConfigParser(interpolation=ExtendedInterpolation())
+config.read("/opt/sparkjobs/ml-analytics-service/migrations/releases/report_config.ini")
+
+
+script_path = config.get("REPORTS_FILEPATH","script_path")
+sys.path.insert(0, script_path)
+from mongo_log import *
+
+# Required field gathering for API
+base_url = config.get("API_ENDPOINTS","base_url")
+headers_api = {
+        'Content-Type': config.get("API_HEADERS", "content_type"),
+        'Authorization' : config.get("API_HEADERS","authorization_access_token")
+    }
+
+folder_path = config.get("REPORTS_FILEPATH","folder_config")
+
+
+# open the csv from the folder and return report details
+def returnReports(filePath,reportType):
+    returnResult = []
+    doc = {}
+    if reportType == "frontend":
+        key = "reports_tag"
+    elif reportType == "backend":
+        key = "reports_id"
+    try:
+        csvFileReader = csv.DictReader(open(filePath,'rt'))
+        for read in csvFileReader:
+            data = json.dumps(read)
+            data = json.loads(data)
+            reportId = data[key].lstrip().rstrip()
+            returnResult.append(reportId)
+            return returnResult
+    except Exception as exception :
+        doc["operation"] = "fetch_retire_reports"
+        doc["errmsg"] = "Exception message {}: {}".format(type(exception).__name__, exception)
+        typeErr = "exception"
+        insert_doc(doc,typeErr)
+        return False
+
+
+# hit the api to retire the frontend reports 
+def frontend_retire(access_token,tag):
+    reportsLookUp = fetchAllReports()
+
+    doc = {
+                "reportTag" : tag,
+                "operation": "frontEnd_retire"
+            }
+
+    headers_api["x-authenticated-user-token"] = access_token
+
+    try:
+        url_frontend_retire = base_url + config.get("API_ENDPOINTS","frontend_retire") + str(reportsLookUp[tag])
+        response_api = requests.delete(
+                   url_frontend_retire,
+                   headers=headers_api
+                )
+    
+        doc["reportId"] = str(reportsLookUp[tag])
+
+        if response_api.status_code == 200:
+            typeErr = "crud"
+        else:
+            doc["errmsg"] = str(response_api.status_code)  + response_api.text
+            typeErr = "error"
+    
+    except Exception as exception:
+        doc["errmsg"] = "Exception message {}: {}".format(type(exception).__name__, exception)
+        typeErr = "exception"
+
+    insert_doc(doc,typeErr)
+
+
+# hit the api to retire the backend reports 
+def backend_retire(reportId):
+    doc = {
+                "reportId" : reportId,
+                "operation": "backEnd_retire"
+            }
+    
+    try:
+        url_backend_retire = base_url + config.get("API_ENDPOINTS","backend_retire") + str(reportId)
+        response_api = requests.patch(
+                   url_backend_retire,
+                   headers=headers_api
+                )
+    
+        if response_api.status_code == 200:
+            typeErr = "crud"
+        else:
+            doc["errmsg"] = str(response_api.status_code)  + response_api.text
+            typeErr = "error"
+    except Exception as exception:
+        doc["errmsg"] = "Exception message {}: {}".format(type(exception).__name__, exception)
+        typeErr = "exception"
+
+    insert_doc(doc,typeErr)
