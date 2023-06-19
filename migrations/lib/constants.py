c0e636b561869488c9c907339e8602bd0102a989
Ashwiniev95 <ashwini.ev@tibilsolutions.com>
2023-06-19 21:34:25 +0530

    update logic & add constant file

---
+++ migrations/lib/constants.py
@@ -0,0 +1,3 @@
+success_code = 200
+success_code1 = 201
+config_path = "/opt/sparkjobs/ml-analytics-service/config.ini"
--- migrations/lib/update_tag.py
+++ migrations/lib/update_tag.py
@@ -2,21 +2,20 @@ import requests
 import os, json,sys
 from configparser import ConfigParser,ExtendedInterpolation
 
-sys.path.insert(0, config.get("REPORTS_FILEPATH","script_path"))
-from mongo_logging import *
-from get_token import get_access_token
-
 # Read the Config
 config_path = os.path.split(os.path.dirname(os.path.abspath(__file__)))
 config = ConfigParser(interpolation=ExtendedInterpolation())
-config.read(config_path[0] + "/report_config.ini")
+config.read("/opt/sparkjobs/ml-analytics-service/config.ini")
+
+sys.path.insert(0, config.get("REPORTS_FILEPATH","script_path"))
+
+from mongo_log import *
+from get_token import get_access_token
+import constants
 
 response_api = get_access_token()
-if response_api["status_code"] == 200:
+if response_api["status_code"] == constants.success_code:
    access_token = response_api["result"]["access_token"]
-
-
-
 
 # Required field gathering for API
 base_url = config.get("API_ENDPOINTS","base_url")
@@ -27,62 +26,60 @@ headers_api = {
     }
 
 
-doc = {}
-
-def get_report(arr):
-    try :
-         for id in arr:
+def get_report(report_ids):
+   doc = {
+            "operation": "get_report"
+           }
+   try :
+         for id in report_ids:
             url_frontend_get = base_url + config.get("API_ENDPOINTS","frontend_get") + id
             response_api = requests.get(
                              url_frontend_get,
                              headers=headers_api
                            )
-#             doc = {
-#                               "configFileName" : file_name,
-#                               "config" : json.dumps(json_config),
-#                               "operation": "frontend_create"
-#                            }
-            if response_api.status_code == 200:
+
+            if response_api.status_code == constants.success_code:
                json_config = response_api.json()
                json_config = json_config["result"]["reports"][0]
-               type = "crud"
+               doc["config"] : json.dumps(json_config)
+               response_type = "crud"
                update_report(json_config,id)
             else :
                doc["errmsg"] = str(response_api.status_code)  + response_api.text
-               type = "error"
+               response_type = "error"
 
 
-    except Exception as exception:
+   except Exception as exception:
          doc["errmsg"] = "Exception message {}: {}".format(type(exception).__name__, exception)
-         type = "exception"
+         response_type = "exception"
+   insert_doc(doc,response_type)
 
 # Creation of chart using Json config making an API call
 def update_report(json_config,id):
-    doc = {}
+    doc = {
+            "operation": "frontend_update",
+            "reportid" : json_config["reportid"],
+            "title" : json_config["reportconfig"]["title"]
+           }
+    tag = json_config["title"].replace(" ","_")
+    update_config = {"request":{}}
     json_config["createdby"] = config.get("JSON_VARIABLE","createdBy")
-    key_arr = ["children","templateurl","accesspath","visibilityflags"]
+    key_arr = ["children","templateurl","accesspath","visibilityflags","reportid","tags"]
     for key in key_arr:
        del json_config[key]
     try :
         headers_api["x-authenticated-user-token"] = access_token
         url_frontend_update = base_url + config.get("API_ENDPOINTS","frontend_update") + id
-
-        doc = {
-#                  "configFileName" : file_name,
-                  "config" : json.dumps(json_config),
-                  "operation": "frontend_update",
-                  "reportid" : json_config["reportid"],
-                  "title" : json_config["reportconfig"]["title"]
-               }
-
-
+        doc["config"] : json.dumps(json_config)
+        update_config["request"]["report"] =  json_config
+        update_config["request"]["report"]["tags"] =  [tag]
         response_api = requests.patch(
                    url_frontend_update,
-                   data= json.dumps(json_config),
+                   data= json.dumps(update_config),
                    headers=headers_api
                 )
 
-        if response_api.status_code == 200:
+        if response_api.status_code == constants.success_code:
             doc["api_response"] = response_api.json()
             typeErr = "crud"
         else:
@@ -95,7 +92,6 @@ def update_report(json_config,id):
         typeErr = "exception"
 
     insert_doc(doc,typeErr)
-    data_file.close
 
-report_ids = config.get("REPORT_IDS","update_tag")
+report_ids = config.get("REPORT_IDS","update_tag").split(',')
 get_report(report_ids)
