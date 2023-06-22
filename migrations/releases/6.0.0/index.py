63dcb96357c0958c1b4d12d80f0ceae867b02dce
adithya_dinesh <adithya@shikshalokam.org>
2023-06-22 12:21:05 +0530

    Index modified according to execution flow

--- migrations/releases/6.0.0/index.py
+++ migrations/releases/6.0.0/index.py
@@ -6,11 +6,9 @@ root_path = "/opt/sparkjobs/ml-analytics-service/"
 config = ConfigParser(interpolation=ExtendedInterpolation())
 config.read(root_path + "config.ini")
 
-
 base_path = os.getcwd()
 
 sys.path.insert(0, root_path + "migrations/lib")
-
 
 from get_token import get_access_token
 from create import backend_create,frontend_create
@@ -32,37 +30,34 @@ frontend_create_files = os.listdir(base_path + "/config/frontend/create")
 backend_update_files = os.listdir( base_path + "/config/backend/update/")
 frontend_update_files = os.listdir(base_path + "/config/frontend/update/")
 
-
 # json file for retire 
 backend_retire_reports = os.listdir(base_path + "/config/backend/retire/")
 frontend_retire_reports = os.listdir(base_path + "/config/frontend/retire/")
 
-
+# STEP 1 
 # call function to update tags of ml reports
 update_tag()
 
+# STEP 2 
 # calling retire function for chart disabling    
 backend_retire(backend_retire_reports,base_path)
 
-
+# STEP 3
 #calling create function for chart creation
 for file in backend_create_files:
   backend_create(file,base_path)
 
-#calling update function for chart updation
-for file in backend_update_files:
-   backend_update(file,base_path)
 
 #calling create function for report creation
 if (access_token!= None):
-   
-   # #calling create function for report creation   
-   for file in frontend_create_files:
-      frontend_create(access_token,file,base_path)
-   
-   # calling retire function for report disabling    
-   frontend_retire(access_token,frontend_retire_reports,base_path)
 
+   # STEP 4
    # calling update function for report updation
    for file in frontend_update_files:
       frontend_update(access_token,file,base_path)
+
+   # STEP 5 
+   # calling create function for report creation   
+   for file in frontend_create_files:
+      frontend_create(access_token,file,base_path)
+   
