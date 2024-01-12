e65b5af8a25cbc08bf0cf0d3f4fe5a9519eb2c9e
adithya_dinesh <adithya@shikshalokam.org>
2024-01-12 13:13:20 +0530

    NVSK file rename changes

--- urgent_data_metrics/imp_project_metrics.py
+++ urgent_data_metrics/imp_project_metrics.py
@@ -370,7 +370,7 @@ final_json = {
 with open(os.path.join(local_path,'micro_improvement.json'), 'w') as json_file:
     json.dump(final_json, json_file, indent=2) 
 
-os.rename(f'{path}' + f'{result[0]}', f'{path}' + 'micro_improvement.csv')
+os.rename(os.path.join(local_path,'data.csv'), f'{local_path}' + 'micro_improvement.csv')
 # Uploading file to Cloud
 cloud_init.upload_to_NVSK_cloud(blob_Path = "Manage_Learn_Data/micro_improvement", local_Path = local_path, file_Name = 'micro_improvement.json')
 
