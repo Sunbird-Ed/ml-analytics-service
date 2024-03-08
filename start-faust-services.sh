444ad90687f4c9d55ef603edb03e06cba5c0e0a1
vivek <user@PWA177.local>
2024-03-08 23:36:01 +0530

    removed start-faust-services.sh

--- start-faust-services.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#start survey_realtime_streaming
-/opt/sparkjobs/faust_as_service/faust.sh  survey/survey_realtime_streaming survey/ &
-
-wait -n
-
-exit $?
--- start-services.sh
+++ start-services.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+#start survey_realtime_streaming
+/opt/sparkjobs/faust_as_service/faust.sh  survey/survey_realtime_streaming survey/ &
+
 #start observations
-/opt/sparkjobs/faust_as_service/faust.sh  observations/py_observation_streaming observations/ &
+#/opt/sparkjobs/faust_as_service/faust.sh  observations/py_observation_streaming observations/ &
 
 #start observation_evidence
-/opt/sparkjobs/faust_as_service/faust.sh  observations/py_observation_evidence_streaming observations/ &
+#/opt/sparkjobs/faust_as_service/faust.sh  observations/py_observation_evidence_streaming observations/ &
 
 #start survey
-/opt/sparkjobs/faust_as_service/faust.sh  survey/py_survey_streaming survey/ &
+#/opt/sparkjobs/faust_as_service/faust.sh  survey/py_survey_streaming survey/ &
 
 #start survey_evidence
-/opt/sparkjobs/faust_as_service/faust.sh  survey/py_survey_evidence_streaming survey/ &
+#/opt/sparkjobs/faust_as_service/faust.sh  survey/py_survey_evidence_streaming survey/ &
 
 wait -n
 
