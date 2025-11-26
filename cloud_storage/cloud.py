b01bc5fb4164420ffdf8f67249a2a955bfc3b359
Vivek M <125434153+Vivek-M-08@users.noreply.github.com>
2025-11-26 17:22:17 +0530

    updated mongo and cloud configs

--- cloud_storage/cloud.py
+++ cloud_storage/cloud.py
@@ -90,6 +90,6 @@ class MultiCloud:
         accessKey = config.get("ORACLE", "access_key"),
         secretAccessKey = config.get("ORACLE", "secret_access_key"),
         endpoint_url = config.get("ORACLE", "endpoint_url"),
-        bucketName = "dev-diksha-privatereports"
+        bucketName = config.get("ORACLE", "bucket_name")
                 )     
         oracle_service.upload_files(bucketPath = blob_Path, localPath = local_Path, fileName = file_Name)
